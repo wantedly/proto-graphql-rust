bd6ffe0542dff23fa3c6132cf5fafb71665fdebf
Taiki Endo <te316e89@gmail.com>
2021-10-02 15:41:02 +0900

    Remove --experimental_allow_proto3_optional

--- examples/subscription/build.rs
+++ examples/subscription/build.rs
@@ -1,8 +1,7 @@
 #![warn(rust_2018_idioms)]
 
 fn main() {
-    let mut config = proto_graphql_build::Config::new();
-    config.protoc_arg("--experimental_allow_proto3_optional");
+    let config = proto_graphql_build::Config::new();
     proto_graphql_build::configure()
         .out_dir("src/generated")
         .compile_with_config(config, &["proto/subscription.proto"], &["proto"])
