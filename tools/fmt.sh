59f1aed5bd35e2b325348a5939cf8ca33b00f7de
Taiki Endo <te316e89@gmail.com>
2022-12-16 21:18:52 +0900

    Update CI config

--- tools/fmt.sh
+++ tools/fmt.sh
@@ -13,21 +13,19 @@ cd "$(cd "$(dirname "$0")" && pwd)"/..
 # shellcheck disable=SC2046
 if [[ -z "${CI:-}" ]]; then
     cargo fmt --all
-    if shfmt --version &>/dev/null; then
+    if type -P shfmt &>/dev/null; then
         shfmt -l -w $(git ls-files '*.sh')
     fi
-    if prettier --version &>/dev/null; then
-        prettier -l -w $(git ls-files '*.yml')
-        prettier -l -w $(git ls-files '*.js')
+    if type -P npm &>/dev/null; then
+        npx prettier -l -w $(git ls-files '*.yml') $(git ls-files '*.js')
     fi
-    if clang-format --version &>/dev/null; then
+    if type -P clang-format &>/dev/null; then
         clang-format -i $(git ls-files '*.proto')
     fi
 else
     cargo fmt --all --check
     shfmt -d $(git ls-files '*.sh')
-    prettier -c $(git ls-files '*.yml')
-    prettier -c $(git ls-files '*.js')
+    npx prettier -c $(git ls-files '*.yml') $(git ls-files '*.js')
     clang-format -i $(git ls-files '*.proto')
     git diff --exit-code
 fi
